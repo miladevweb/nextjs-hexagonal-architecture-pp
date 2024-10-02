eb683f55ea17e64cdb2ef19428215668d9af9862
miladevweb <miladevweb@outlook.com>
2024-10-02 12:57:57 -0500

    comment localstoragerepository

--- lib/post/infrastructure/Repositories/LocalStoragePostRepository/index.ts
+++ lib/post/infrastructure/Repositories/LocalStoragePostRepository/index.ts
@@ -1,40 +1,40 @@
-import { User } from '../../../domain/user'
-import { IUserRepository } from '../../../domain/repository/user-repository'
+// import { User } from '../../../domain/user'
+// import { IUserRepository } from '../../../domain/repository/user-repository'
 
-export function localStorageUserRepository(): IUserRepository {
-  return {
-    getAll: () => {
-      const users = localStorage.getItem('users')
-      const parsedUsers = users ? (JSON.parse(users) as User[]) : []
-      return Promise.resolve(parsedUsers)
-    },
+// export function localStorageUserRepository(): IUserRepository {
+//   return {
+//     getAll: () => {
+//       const users = localStorage.getItem('users')
+//       const parsedUsers = users ? (JSON.parse(users) as User[]) : []
+//       return Promise.resolve(parsedUsers)
+//     },
 
-    save: (user: User) => {
-      const users = localStorage.getItem('users')
-      const parsedUsers = users ? (JSON.parse(users) as User[]) : []
+//     save: (user: User) => {
+//       const users = localStorage.getItem('users')
+//       const parsedUsers = users ? (JSON.parse(users) as User[]) : []
 
-      const userExists = parsedUsers.findIndex((u) => u.id === user.id)
+//       const userExists = parsedUsers.findIndex((u) => u.id === user.id)
 
-      // edit
-      if (userExists !== -1) {
-        parsedUsers[userExists] = user
-      }
-      // create
-      else {
-        parsedUsers.push(user)
-      }
+//       // edit
+//       if (userExists !== -1) {
+//         parsedUsers[userExists] = user
+//       }
+//       // create
+//       else {
+//         parsedUsers.push(user)
+//       }
 
-      localStorage.setItem('users', JSON.stringify(parsedUsers))
-      return Promise.resolve()
-    },
+//       localStorage.setItem('users', JSON.stringify(parsedUsers))
+//       return Promise.resolve()
+//     },
 
-    delete: (id: string) => {
-      const users = localStorage.getItem('users')
-      const parsedUsers = users ? (JSON.parse(users) as User[]) : []
-      const newUsers = parsedUsers.filter((user) => user.id !== id)
+//     delete: (id: string) => {
+//       const users = localStorage.getItem('users')
+//       const parsedUsers = users ? (JSON.parse(users) as User[]) : []
+//       const newUsers = parsedUsers.filter((user) => user.id !== id)
 
-      localStorage.setItem('users', JSON.stringify(newUsers))
-      return Promise.resolve()
-    },
-  }
-}
+//       localStorage.setItem('users', JSON.stringify(newUsers))
+//       return Promise.resolve()
+//     },
+//   }
+// }
